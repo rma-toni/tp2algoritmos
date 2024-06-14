4a5c0a3ca4f2025caeb94b6e55ac250f09e2241f
rma-toni <rmaflores.sw@gmail.com>
2024-06-14 00:11:24 -0300

    Ejercicio 2 terminado

--- main.cpp
+++ main.cpp
@@ -10,17 +10,34 @@ int main(int argc, char const *argv[])
     /*Ingrese un valor N (<25). Generar un arreglo de N componentes en el cual las mismas contengan los primeros numeros naturales
     pares e imprimirlo*/
 
-    int n1;
+    /*int n1;
     cout << "Ingrese un numero menor a 25: ";
     cin >> n1;
-    while (n1>=25)
+    //compruebo que el numero ingresado sea menor a 25 y mayor que 0
+    while (n1>=25 || n1<1)
     {
         cout << "El numero ingresado es igual om mayor a 25, por favor ingrese uno menor: ";
         cin >> n1;
     }
 
-    arrayPar(n1);
+    ejercicio1(n1);*/
     
     //------------------------------EJERCICIO 2-----------------------------
+
+    /*Ingresar un valor entero N(<30) y a continuacion un conjunto de N elementos. Si el ultimo elemento del conjunto tiene un valor menor que 10 
+    imprimir los negativos y en caso contrario los demas*/
+
+    int n2;
+    cout << "Ingrese un numero menor a 30: ";
+    cin >> n2;
+    //compruebo que el numero ingresado sea menor a 25 y mayor que 0
+    while (n2>=30 || n2<0)
+    {
+        cout << "El numero ingresado es invalido, por favor ingrese uno menor a 30 y mayor que 0: ";
+        cin >> n2;
+    }
+
+    ejercicio2(n2);
+
     return 0;
 }
--- tp2.hpp
+++ tp2.hpp
@@ -5,11 +5,33 @@
 /*Ingrese un valor N (<25). Generar un arreglo de N componentes en el cual las mismas contengan los primeros numeros naturales
 pares e imprimirlo*/
 
-void arrayPar(int n){
+void ejercicio1(int n){
     int paresNaturales[n];
     for (int i = 1; i <= n; i++)
     {
         paresNaturales[i-1] = i*2;
         std::cout << paresNaturales[i-1] << std::endl;
     }    
+}
+
+//---------------------------------------------------------EJERCICIO 2--------------------------------------------------------
+
+/*Ingresar un valor entero N(<30) y a continuacion un conjunto de N elementos. Si el ultimo elemento del conjunto tiene un valor menor que 10 
+imprimir los negativos y en caso contrario los demas*/
+
+void ejercicio2(int n){
+    int array1[n];
+    //Se procede al ingreso de valores por consola
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "Ingrese el elemento numero " << i+1 << ": ";
+        std::cin >> array1[i];
+    }
+    //Si es menor a 10 se imprime negativos
+    if (array1[n-1] < 10)
+    {
+        for (int i = 0; i < n; i++) if(array1[i] < 0) std::cout << array1[i] << " ";  
+    }else{
+        for (int i = 0; i < n; i++) if(array1[i] >= 0) std::cout << array1[i] << " ";
+    }
 }
